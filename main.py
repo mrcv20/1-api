4b7facacb8c703f0543f2a0ed8a20a66ebbb8531
Marcos <marcos.valentee20@gmail.com>
2021-01-28 14:11:10 -0400

    testing a few of features

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
-from flask import Flask
-from flask_restful import Api, Resource
+from flask import Flask, request
+from flask_restful import Api, Resource, reqparse
 
 
 app = Flask(__name__) # instancia da class Flask "app"
-
 api = Api(app) # instância da class Api "api"
 
-class HelloWorld(Resource): # herdando de Resource para a criação de funções que serão mapeadas em metodos HTTP
-    def get(self): 
-        return {"data":"Hello World"}
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
+video_put_args.add_argument("views", type=str, help="Views of the videois requried", required=True)
+video_put_args.add_argument("likes", type=str, help="Likes of the video is required", required=True)
+videos = {}
 
-api.add_resource(HelloWorld, "/helloworld") # registrando a class HelloWorld como Resource, definindo sua endpoint "helloworld"
+class Video(Resource):
+    def get(self, video_id):
+        return videos[video_id]
 
+    def put(self, video_id):
+        args = video_put_args.parse_args()
+        return {video_id: args}
+
+api.add_resource(Video, "/video/<int:video_id>")
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -2,5 +2,5 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "helloworld") # enviando requisição get para URL
-print(response.json()) # usando json para receber response com algumas informações ao invez de um objeto
+response = requests.put(BASE + "video/1", {"likes": 10}) # enviando requisição get para endpoint
+print(response.json()) # usando json para receber response com algumas informações ao i nvez de um objeto
